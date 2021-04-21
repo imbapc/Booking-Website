007b8e6920d1ceed0cd3a705140063de018b60c2
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-21 18:28:21 +1200

    Init domain model

--- concert-service/src/main/java/asg/concert/service/domain/Seat.java
+++ concert-service/src/main/java/asg/concert/service/domain/Seat.java
@@ -1,11 +1,16 @@
 package asg.concert.service.domain;
 
+import asg.concert.common.dto.SeatDTO;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 public class Seat {
@@ -70,5 +75,30 @@ public class Seat {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seat seat = (Seat) o;
+
+        return new EqualsBuilder()
+                .append(label, seat.label)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(label)
+                .toHashCode();
     }
 }
