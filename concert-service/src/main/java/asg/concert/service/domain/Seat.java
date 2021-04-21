f68da62bc5aa16dbf43be67cadcfb7cdde6c0d33
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-21 18:20:44 +1200

    Init domain model

--- concert-service/src/main/java/asg/concert/service/domain/Seat.java
+++ concert-service/src/main/java/asg/concert/service/domain/Seat.java
@@ -1,18 +1,74 @@
 package asg.concert.service.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
+@Entity
 public class Seat {
 
-    // TODO Implement this class.
+    @Id
+    @GeneratedValue
+    private long id;
 
-	public Seat() {}
+    @Column(name = "LABEL")
+    private String label;
 
-	public Seat(String label, boolean isBooked, LocalDateTime date, BigDecimal price) {
+    @Column(name = "IS_BOOKED")
+    private boolean isBooked;
 
-	}
+    @Column(name = "DATE")
+    private LocalDateTime date;
 
+    @Column(name = "PRICE")
+    private BigDecimal price;
+
+    public Seat() {
+    }
+
+    public Seat(String label, boolean isBooked, LocalDateTime date, BigDecimal price) {
+        this.label = label;
+        this.isBooked = isBooked;
+        this.date = date;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
