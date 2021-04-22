04bb963e5441e506ce0bcdad969b0bcce0fa8d33
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-23 08:15:21 +1200

    Refactor GET method to retrieve concert by id

--- concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
@@ -5,6 +5,7 @@ import asg.concert.service.domain.Concert;
 import asg.concert.service.mapper.ConcertMapper;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -18,20 +19,26 @@ public class ConcertResource {
     @Path("concerts/{id}")
     public Response retrieveConcert(@PathParam("id") long id) {
         EntityManager em = PersistenceManager.instance().createEntityManager();
+        EntityTransaction tx = null;
         Concert concert;
         ConcertDTO dtoConcert;
         try {
-            em.getTransaction().begin();
-
+            tx = em.getTransaction();
+            tx.begin();
             concert = em.find(Concert.class, id);
             if (concert == null) {
-                throw new WebApplicationException(Response.Status.NOT_FOUND);
+                return Response.status(Response.Status.NOT_FOUND).build();
             }
-
-            em.getTransaction().commit();
             dtoConcert = ConcertMapper.toDto(concert);
+            tx.setRollbackOnly();
+            tx.commit();
         } finally {
-            em.close();
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
         }
         return Response.ok().entity(dtoConcert).build();
     }
