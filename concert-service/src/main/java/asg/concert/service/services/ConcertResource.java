72e30cf0c1a9bd2054e47e6d48dd05d18d72c6c3
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-25 12:58:14 +1200

    1. Add retrieve performers and login resources 2. Add @version annotation to User

--- concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/asg/concert/service/services/ConcertResource.java
@@ -3,18 +3,23 @@ package asg.concert.service.services;
 import asg.concert.common.dto.ConcertDTO;
 import asg.concert.common.dto.ConcertSummaryDTO;
 import asg.concert.common.dto.PerformerDTO;
+import asg.concert.common.dto.UserDTO;
 import asg.concert.service.domain.Concert;
 import asg.concert.service.domain.Performer;
+import asg.concert.service.domain.User;
 import asg.concert.service.mapper.ConcertMapper;
 import asg.concert.service.mapper.PerformerMapper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Path("/concert-service")
 @Produces(MediaType.APPLICATION_JSON)
@@ -94,6 +99,28 @@ public class ConcertResource {
     }
 
     @GET
+    @Path("/performers")
+    public Response retrievePerformers() {
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        List<Performer> allPerformers;
+        List<PerformerDTO> result = new ArrayList<>();
+        try {
+            allPerformers = em.createQuery("SELECT p FROM Performer p").getResultList();
+            if (allPerformers == null) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            for (Performer performer : allPerformers) {
+                result.add(PerformerMapper.toPerformerDto(performer));
+            }
+        } finally {
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
+        }
+        return Response.ok().entity(result).build();
+    }
+
+    @GET
     @Path("/performers/{id}")
     public Response retrievePerformerById(@PathParam("id") long id) {
         EntityManager em = PersistenceManager.instance().createEntityManager();
@@ -121,4 +148,46 @@ public class ConcertResource {
         return Response.ok().entity(dtoPerformer).build();
     }
 
+    @POST
+    @Path("/login")
+    public Response login(UserDTO userDTO) {
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        EntityTransaction tx = null;
+        List<User> userResults;
+        User matchedUser;
+        // Test if user exist
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            TypedQuery<User> userQuery = em
+                    .createQuery(
+                            "select u from User u where u.username = :username",
+                            User.class)
+                    .setParameter("username", userDTO.getUsername());
+            userResults = userQuery.getResultList();
+            if (userResults.isEmpty()) {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            } else {
+                matchedUser = userResults.get(0);
+            }
+            tx.setRollbackOnly();
+            tx.commit();
+        } finally {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
+        }
+
+        // Test user password
+        if (!userDTO.getPassword().equals(matchedUser.getPassword())) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        // Generate cookie and return Ok
+        NewCookie cookie = new NewCookie("auth", UUID.randomUUID().toString());
+        return Response.ok().cookie(cookie).build();
+    }
 }
