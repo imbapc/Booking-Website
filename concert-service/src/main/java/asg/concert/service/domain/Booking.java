a22ca9854becae14f0ae5bf1c0b348abef7f7721
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-26 16:29:44 +1200

    Add Booking content

--- concert-service/src/main/java/asg/concert/service/domain/Booking.java
+++ concert-service/src/main/java/asg/concert/service/domain/Booking.java
@@ -1,6 +1,9 @@
 package asg.concert.service.domain;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "BOOKINGS")
@@ -10,6 +13,56 @@ public class Booking {
     @GeneratedValue
     private long id;
 
+    private long concertId;
+    private LocalDateTime date;
+    private String bookingUser;
+
+    @OneToMany(cascade = {CascadeType.PERSIST})
+    private List<Seat> seats = new ArrayList<>();
+
     public Booking() {
+    }
+
+    public Booking(long concertId, LocalDateTime date, String bookingUser, List<Seat> seats) {
+        this.concertId = concertId;
+        this.date = date;
+        this.bookingUser = bookingUser;
+        this.seats = seats;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getConcertId() {
+        return concertId;
+    }
+
+    public void setConcertId(long concertId) {
+        this.concertId = concertId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public String getBookingUser() {
+        return bookingUser;
+    }
+
+    public void setBookingUser(String bookingUser) {
+        this.bookingUser = bookingUser;
     }
 }
