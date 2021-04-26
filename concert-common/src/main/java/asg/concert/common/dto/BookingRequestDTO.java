ff2c3adb1e8189a825fc210021b14eb5538b8cb7
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-26 16:30:46 +1200

    Add annotation for BookingRequestDTO LocalDateTime

--- concert-common/src/main/java/asg/concert/common/dto/BookingRequestDTO.java
+++ concert-common/src/main/java/asg/concert/common/dto/BookingRequestDTO.java
@@ -1,5 +1,10 @@
 package asg.concert.common.dto;
 
+import asg.concert.common.jackson.LocalDateTimeDeserializer;
+import asg.concert.common.jackson.LocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +21,8 @@ public class BookingRequestDTO {
     private LocalDateTime date;
     private List<String> seatLabels = new ArrayList<>();
 
-    public BookingRequestDTO(){}
+    public BookingRequestDTO() {
+    }
 
     public BookingRequestDTO(long concertId, LocalDateTime date) {
         this.concertId = concertId;
@@ -37,6 +43,8 @@ public class BookingRequestDTO {
         this.concertId = concertId;
     }
 
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     public LocalDateTime getDate() {
         return date;
     }
