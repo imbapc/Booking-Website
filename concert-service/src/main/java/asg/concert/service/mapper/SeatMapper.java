efd660398e922f5fcb1c584c7b94e53c6621456c
jiakai.li <jli488@aucklanduni.ac.nz>
2021-04-26 16:31:12 +1200

    Add SeatMapper

---
+++ concert-service/src/main/java/asg/concert/service/mapper/SeatMapper.java
@@ -0,0 +1,10 @@
+package asg.concert.service.mapper;
+
+import asg.concert.common.dto.SeatDTO;
+import asg.concert.service.domain.Seat;
+
+public class SeatMapper {
+    public static SeatDTO toSeatDto(Seat seat) {
+        return new SeatDTO(seat.getLabel(), seat.getPrice());
+    }
+}
