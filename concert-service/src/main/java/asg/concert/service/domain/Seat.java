cbc830c9405810e017d8d8ea3a1b8fa56d1ba061
imbapc <45018264+imbapc@users.noreply.github.com>
2021-04-27 20:13:13 +1200

    identity seat vars

--- concert-service/src/main/java/asg/concert/service/domain/Seat.java
+++ concert-service/src/main/java/asg/concert/service/domain/Seat.java
@@ -8,11 +8,23 @@ import java.util.Objects;
 public class Seat {
 
     // TODO Implement this class.
+	private String label;
+	
+	private boolean isBooked;
+	
+	private LocalDateTime date;
+	
+	private BigDecimal price;
+	
+	
 
 	public Seat() {}
 
 	public Seat(String label, boolean isBooked, LocalDateTime date, BigDecimal price) {
-
+		this.label = label;
+		this.isBooked = isBooked;
+		this.date = date;
+		this.price = price;
 	}
 
 }
