508639cb2a97321a39643c77a9e0d7a2a8e53b51
hhon669 <hhon669@BCLJ01.uoa.auckland.ac.nz>
2021-05-04 15:11:37 +1200

    Setup Communication between DTO and domainModel

---
+++ concert-service/src/main/java/asg/concert/service/services/ConcertMapper.java
@@ -0,0 +1,27 @@
+package asg.concert.service.services;
+
+import asg.concert.domain.Concert;
+
+public class ConcertMapper() {
+	
+	static Concert toDomainModel(asg.concert.common.dto.ConcertDTO concertDTO) {
+		Concert concert = new Concert(concertDTO.getTitle(),
+				concertDTO.getImageName(),
+				concertDTO.getBlurb(),
+				concertDTO.getDates(),
+				concertDTO.getPerformers());
+		concert.setId(concertDTO.getId);
+		return concert;
+	}
+	
+	static asg.concert.common.dto.ConcertDTO toDTO(Concert concert){
+		asg.concert.common.dto.ConcertDTO concertDTO = new asg.concert.common.dto.ConcertDTO(concert.getId(),
+				concert.getTitle(),
+				concert.getImageName(),
+				concert.getBlurb(),
+				);
+		concertDTO.setDates(concert.getDates);
+		concertDTO.setPerformers(concert.getPerformers);
+		return concertDTO;
+	}
+}
