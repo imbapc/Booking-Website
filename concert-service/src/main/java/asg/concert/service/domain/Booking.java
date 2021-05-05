d61c1a8e60d6e4daccc080a526bdd528682db686
Frederick Hy <frederickhy001@gmail.com>
2021-05-05 21:49:47 +1200

    Revert booking

--- concert-service/src/main/java/asg/concert/service/domain/Booking.java
+++ concert-service/src/main/java/asg/concert/service/domain/Booking.java
@@ -1,102 +1,15 @@
 package asg.concert.service.domain;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import javax.persistence.*;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-/**
- * A booking for a concert
- */
 @Entity
 @Table(name = "BOOKINGS")
 public class Booking {
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	
-	@ManyToOne
-	private User user;
-	
-	private long concertId;
-	private LocalDateTime date;
 
-	@OneToMany(fetch = FetchType.EAGER)
-	private List<Seat> seats = new ArrayList<>();
+    @Id
+    @GeneratedValue
+    private long id;
 
-	public Booking() {
-	}
-
-	public Booking(User user, long concertId, LocalDateTime date) {
-		this.user = user;
-		this.concertId = concertId;
-		this.date = date;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public long getConcertId() {
-		return concertId;
-	}
-
-	public void setConcertId(long concertId) {
-		this.concertId = concertId;
-	}
-
-	public LocalDateTime getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDateTime date) {
-		this.date = date;
-	}
-
-	public List<Seat> getSeats() {
-		return seats;
-	}
-
-	public void setSeats(List<Seat> seats) {
-		this.seats = seats;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(concertId, date, id, user);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Booking)) {
-			return false;
-		}
-		Booking other = (Booking) obj;
-		return concertId == other.concertId && Objects.equals(date, other.date) && Objects.equals(id, other.id) && Objects.equals(user, other.user);
-	}
+    public Booking() {
+    }
 }
