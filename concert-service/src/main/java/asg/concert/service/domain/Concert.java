478ea8438d7e3cc2913888a0167631f0f07ea082
Frederick Hy <frederickhy001@gmail.com>
2021-05-06 15:31:15 +1200

    add

--- concert-service/src/main/java/asg/concert/service/domain/Concert.java
+++ concert-service/src/main/java/asg/concert/service/domain/Concert.java
@@ -59,6 +59,10 @@ public class Concert {
     public long getId() {
         return id;
     }
+    
+    public void setId(long id) {
+    	this.id = id;
+    }
 
     public String getTitle() {
         return title;
